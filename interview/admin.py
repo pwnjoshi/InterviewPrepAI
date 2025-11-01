2bc38144abf2a9ab5e864dd0b7f138a58ec3228c
pwnjoshi <joshipawan2021@gmail.com>
2025-11-01 22:15:52 +0530

    Minor fixes

--- interview/admin.py
+++ interview/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import CandidateProfile, Question, InterviewSession, Answer
 
+admin.site.site_title = "Nexora Admin Portal"
+admin.site.site_header = "Nexora Admin Portal"
+admin.site.index_title = "Welcome to Nexora Admin Portal"
+
 # This tells the admin site to show these models
 admin.site.register(CandidateProfile)
 admin.site.register(Question)
